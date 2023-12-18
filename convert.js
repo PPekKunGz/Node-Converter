90b59cbba103c8de3fa541b258b5f42e4f93ee02
PPekKunGzDev <kittipat25er@gmail.com>
2023-12-18 23:11:25 +0700

    change audio channel & simple rate

--- convert.js
+++ convert.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const path = require('path');
-const ffmpeg = require('fluent-ffmpeg');
+const fs = require("fs");
+const path = require("path");
+const ffmpeg = require("fluent-ffmpeg");
 
 // Directory containing input WAV and MP3 files
-const inputDir = 'Eventsounds.input'; // Change this to your directory path
+const inputDir = "Eventsounds.input"; // Change this to your directory path
 
 // Output directory for converted OGG files
-const outputDir = 'Eventsounds.output'; // Change this to your directory path
+const outputDir = "Eventsounds.output"; // Change this to your directory path
 
 // Check if the output directory exists, if not, create it
 if (!fs.existsSync(outputDir)) {
@@ -16,28 +16,30 @@ if (!fs.existsSync(outputDir)) {
 // Get a list of files in the input directory
 fs.readdir(inputDir, (err, files) => {
   if (err) {
-    console.error('Error reading directory:', err);
+    console.error("Error reading directory:", err);
     return;
   }
 
   // Filter files with .wav or .mp3 extensions
   const audioFiles = files.filter((file) => {
-    return file.endsWith('.wav') || file.endsWith('.mp3');
+    return file.endsWith(".wav") || file.endsWith(".mp3");
   });
 
   // Convert each audio file to OGG format
   audioFiles.forEach((file) => {
     const inputFile = path.join(inputDir, file);
-    const outputFileName = `${path.parse(file).name}.convert.ogg`;
+    const outputFileName = `${path.parse(file).name}_music_sound.ogg`;
     const outputFile = path.join(outputDir, outputFileName);
 
     ffmpeg(inputFile)
-      .outputOptions('-c:a libvorbis') // Set the audio codec to libvorbis (OGG)
-      .toFormat('ogg') // Set the output format to OGG
-      .on('end', () => {
+      .outputOptions("-c:a libvorbis") // Set the audio codec to libvorbis (OGG)
+      .outputOption("-ac", 1) // Set to mono
+      .outputOption("-ar", 44100) // Sample Rate to 44.1 kHz
+      .toFormat("ogg") // Set the output format to OGG
+      .on("end", () => {
         console.log(`Conversion of ${file} finished`);
       })
-      .on('error', (err) => {
+      .on("error", (err) => {
         console.error(`Error converting ${file}:`, err);
       })
       .save(outputFile);
